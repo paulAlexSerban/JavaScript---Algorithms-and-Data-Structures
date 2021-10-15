02d29438103308373a59a2d76165aaed08ab9c10
paulAlexSerban <paul.alex.serban@gmail.com>
2021-10-15 15:58:05 +0300

    js-ads_07 | ADDED | fcc-js - basic-data-structures - objects

---
+++ Basic_Data-Structures/objects/01_key-value-pairs.js
@@ -0,0 +1,10 @@
+/**
+ * At their most basic, objects are just collections of key-value pairs. 
+ * In other words, they are pieces of data (values) mapped to unique identifiers called properties (keys)
+ */
+
+ const tekkenCharacter = {
+  player: 'Hwoarang',
+  fightingStyle: 'Tae Kwon Doe',
+  human: true
+};
