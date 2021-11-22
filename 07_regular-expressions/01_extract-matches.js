1a869f98e3c3463cc6177df49b94f9d2a837a549
paulAlexSerban <paul.alex.serban@gmail.com>
2021-11-22 21:16:14 +0200

    js_ads_0701 | continued regular expressions

--- 07_regular-expressions/01_extract-matches.js
+++ 07_regular-expressions/01_extract-matches.js
@@ -1,5 +1,6 @@
 /**
  * You can extract the actual matches you found with the `.match()` method
+ * To search or extract a pattern more than once, you can use the `g` flag
  */
 
 function extractMatch(string, regex) {
@@ -8,11 +9,17 @@ function extractMatch(string, regex) {
 
 // Example 1
 const helloString = "Hello, World!";
-const helloRegExp = /Hello/;
+const helloRegExp = /Hello/ig;
 
 console.log(extractMatch(helloString, helloRegExp));
 
 // Example 2
 const codingString = "Extract the word 'coding' from this string.";
-const codingRegex = /coding/;
+const codingRegex = /coding/ig;
 console.log(extractMatch(codingString, codingRegex));
+
+// Example 3
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle|twinkleStar/gi;
+
+console.log(extractMatch(twinkleStar, starRegex));
