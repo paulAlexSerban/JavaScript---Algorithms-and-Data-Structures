406d04e32aa17e194305aa25f706149c984046d6
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-08 17:46:43 +0200

    js-ads_0003 | updated random number generator

--- 00_basics-and-fundamentals/11_generateRandomNumbers.js
+++ 00_basics-and-fundamentals/11_generateRandomNumbers.js
@@ -11,9 +11,11 @@ function randomFraction() {
   return Math.random();
 }
 
+console.log("randomFraction", randomFraction());
+
 /**
  * Whole Numbers
- * 
+ *
  * It's great that we can generate random decimal numbers, but it's even more useful if we use it to generate random whole numbers.
  * - Use `Math.random()` to generate a random decimal.
  * - Multiply that random decimal by 20.
@@ -27,22 +29,20 @@ function randomWholeNum() {
   return Math.floor(Math.random() * 10);
 }
 
+console.log("randomWholeNum", randomWholeNum());
+
 /**
  * Whole Numbers with range
  * Instead of generating a random whole number between zero and a given number like we did before, we can generate a random whole number that falls within a range of two specific numbers.
- * To do this, we'll define a minimum number min and a maximum number max.
- * 1. Here's the formula we'll use. Take a moment to read it and try to understand what this code is doing:
- */
 
- Math.floor(Math.random() * (max - min + 1)) + min; /* 1 */
-
- /**
   * that takes a range `myMin` and `myMax` and returns a random whole number that's greater than or equal to `myMin`, and is less than or equal to myMax, inclusive.
   * @param {*} myMin 
   * @param {*} myMax 
   * @returns 
   */
 
- function randomRange(myMin, myMax) {
+function randomRange(myMin, myMax) {
   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
 }
+
+console.log('randomRange',randomRange(13, 15))
