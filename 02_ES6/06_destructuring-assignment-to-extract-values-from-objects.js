1ce03607debcfe4615038857b89ae85cd0dce7d0
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-21 13:31:45 +0300

    added some extra exmaple for es6

--- 02_ES6/06_destructuring-assignment-to-extract-values-from-objects.js
+++ 02_ES6/06_destructuring-assignment-to-extract-values-from-objects.js
@@ -26,24 +26,41 @@ const { name, age } = user;
  *  You can extract as many or few values from the object as you want.
  */
 
- const HIGH_TEMPERATURES = {
+const HIGH_TEMPERATURES = {
   yesterday: 75,
   today: 77,
-  tomorrow: 80
+  tomorrow: 80,
 };
 
-const {today, tomorrow} = HIGH_TEMPERATURES;
+const { today, tomorrow } = HIGH_TEMPERATURES;
 
 /**
- * Destructuring allows you to assign a new variable name when extracting values. 
+ * Destructuring allows you to assign a new variable name when extracting values.
  * You can do this by putting the new name after a colon when assigning the value.
  */
 
- const { name: userName, age: userAge } = user;
+const { name: userName, age: userAge } = user;
 
 /**
- * You may read it as "get the value of user.name and assign it to a new variable named userName" and so on. 
+ * You may read it as "get the value of user.name and assign it to a new variable named userName" and so on.
  * The value of userName would be the string John Doe, and the value of userAge would be the number 34.
  */
 
- const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+const runner = {
+  first: "Eliud",
+  last: "Kipchoge",
+  country: "Kenya",
+  title: "Elder of the Order of the Golden Heart of Kenya",
+};
+
+// const {
+//   first,
+//   last,
+//   time
+// } = runner;
+
+const { country: nation, title: honorific } = runner;
+
+const { first, last, ...other } = runner;
--- 02_ES6/08_destructuring-assignment-to-assign-variables-from-arrays.js
+++ 02_ES6/08_destructuring-assignment-to-assign-variables-from-arrays.js
@@ -14,5 +14,34 @@ console.log(a, b);
 const [c, d, , , , e] = [1, 2, 3, 4, 5, 6];
 console.log(c, d, e);
 
-let f = 8, g = 6;
+let f = 8,
+  g = 6;
 [f, g] = [g, f];
+
+const raceResults = [
+  "Eliud Kipchoge",
+  "Feyisa Lelisa",
+  "Galen Rupp",
+  "Ghirmay Ghebreslassie",
+  "Alphonce Simbu",
+  "Jared Ward",
+];
+
+// The old way:
+// const gold = raceResults[0]
+// const silver = raceResults[1]
+// const bronze = raceResults[2]
+
+// Using Destructuring:
+const [gold, silver, bronze] = raceResults;
+gold; //'Eliud Kipchoge'
+silver; //'Feyisa Lelisa'
+bronze; //'Galen Rupp'
+
+const [first, , , fourth] = raceResults;
+first; //'Eliud Kipchoge'
+fourth; //'Ghirmay Ghebreslassie'
+
+const [winner, ...others] = raceResults;
+winner; //'Eliud Kipchoge'
+others; //["Feyisa Lelisa", "Galen Rupp", "Ghirmay Ghebreslassie", "Alphonce Simbu", "Jared Ward"]
