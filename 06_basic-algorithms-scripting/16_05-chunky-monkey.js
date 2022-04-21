dcb0985609345e4b9d7077d1f047f0fed792b4b3
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-21 19:22:54 +0300

    updated chunky monkey

--- 06_basic-algorithms-scripting/16_05-chunky-monkey.js
+++ 06_basic-algorithms-scripting/16_05-chunky-monkey.js
@@ -3,7 +3,7 @@
  * Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
  */
 
- function chunkArrayInGroups(arr, size) {
+function chunkArrayInGroups(arr, size) {
   if (arr.length <= size) {
     return [arr];
   } else {
@@ -18,4 +18,4 @@ console.log(chunkArrayInGroups(["a", "b", "c", "d", "e", "f", "g", "h"], 2));
  * Code Explanation
  * Array smaller than size is returned nested.
  * For any array larger than size, it is split in two. First segment is nested and concatenated with second segment which makes a recursive call.
-*/
+ */
