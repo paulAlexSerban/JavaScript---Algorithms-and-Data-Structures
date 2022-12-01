c0e626d83b6e0d6b9eb8628cbceec171d6a51c1a
paulAlexSerban <paul.alex.serban@gmail.com>
2022-12-01 11:12:00 +0200

    feat(sliding window): add sliding window exercises

--- 12_computer-science-in-javascript/03_problem-solving/sliding-window/max-subarray-sum_v2.js
+++ 12_computer-science-in-javascript/03_problem-solving/sliding-window/max-subarray-sum_v2.js
@@ -32,3 +32,8 @@ console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4));
 console.log(maxSubarraySum([4, 2, 1, 6], 1));
 console.log(maxSubarraySum([4, 2, 1, 6, 2], 4));
 console.log(maxSubarraySum([], 4));
+console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)); // 39
+console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)); // 5
+console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)); // 5
+console.log(maxSubarraySum([2, 3], 3)); // null
