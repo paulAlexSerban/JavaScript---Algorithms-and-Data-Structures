142e525ea82718bb5f52d8e353d494762fb92cf5
paulAlexSerban <paul.alex.serban@gmail.com>
2022-12-02 18:57:50 +0200

    feat(recursion): add recursion exercises

--- 12_computer-science-in-javascript/04_recursion/factorial.js
+++ 12_computer-science-in-javascript/04_recursion/factorial.js
@@ -6,9 +6,14 @@
 //   return total;
 // };
 
-const factorial = (num) => {
-  if (num === 1) return 1; // define the base case - WARNING: if missing you get into an infinite loop
-  return num * factorial(num - 1);
+const factorial = (x) => {
+  if (x < 0) return 0;
+  if (x <= 1) return 1; // define the base case - WARNING: if missing you get into an infinite loop
+  return x * factorial(x - 1);
 };
 
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 24
 console.log(factorial(6));
+console.log(factorial(7)); // 5040
