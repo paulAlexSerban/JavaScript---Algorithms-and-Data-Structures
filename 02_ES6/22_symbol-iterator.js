827042ab848dcf03598a7cbd7eccb1709d9a12d6
paulAlexSerban <paul.alex.serban@gmail.com>
2022-12-16 14:33:05 +0200

    feat(call apply bind closures): added examples for functional programming

--- 02_ES6/22_symbol-iterator.js
+++ 02_ES6/22_symbol-iterator.js
@@ -26,9 +26,32 @@ const it2 = obj[Symbol.iterator]();
 console.log(it2.next()); // { value: 1, done: false }
 
 // treat the obj objects as iterables
-for(const x of obj) {
-  console.log(x)
+for (const x of obj) {
+  console.log(x);
 }
 // OR
 
-console.log([...obj])
+console.log([...obj]);
+
+// Iterables are only iterated until all bindings are assigned.
+const objOne = {
+  *[Symbol.iterator]() {
+    for (const v of [0, 1, 2, 3]) {
+      console.log(v);
+      yield v;
+    }
+  },
+};
+const [a, b] = objOne; // Only logs 0 and 1
+
+// The rest binding is eagerly evaluated and creates a new array, instead of using the old iterable.
+const objTwo = {
+  *[Symbol.iterator]() {
+    for (const v of [0, 1, 2, 3]) {
+      console.log(v);
+      yield v;
+    }
+  }
+}
+const [x, z, ...rest] = objTwo; // Logs 0 1 2 3
+console.log(rest); // [2, 3] (an array)
