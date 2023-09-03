ea58cd52ae6ec81d87af571f08474f28e763c6af
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-03 16:39:03 +0300

    renamings

--- 12_computer-science-in-javascript/04_recursion/recursiveRange.js
+++ 12_computer-science-in-javascript/04_recursion/recursiveRange.js
@@ -3,7 +3,9 @@
  */
 
 const recursiveRange = (x) => {
-  if (x === 0) return 0;
+  if (x === 0) {
+    return 0;
+  }
   return x + recursiveRange(x - 1);
 };
 
--- 12_computer-science-in-javascript/04_recursion/sumRange.js
+++ 12_computer-science-in-javascript/04_recursion/sumRange.js
@@ -1,5 +1,7 @@
 const sumRange = (num) => {
-  if(num === 1) return 1;
+  if (num === 1) {
+    return 1;
+  }
   return num + sumRange(num - 1);
 }
 
