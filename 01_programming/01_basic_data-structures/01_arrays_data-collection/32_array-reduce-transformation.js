cf0e430dc21b403f172bd6423f8283890548447b
paulAlexSerban <paul.alex.serban@gmail.com>
2023-10-01 13:21:18 +0300

    arrays(intermediate): combination sum

--- 01_programming/01_basic_data-structures/01_arrays_data-collection/32_array-reduce-transformation.js
+++ 01_programming/01_basic_data-structures/01_arrays_data-collection/32_array-reduce-transformation.js
@@ -68,4 +68,19 @@ Constraints:
         }
         return result;
     };
+
+    const nums = [1, 2, 3, 4];
+    const fn = (accum, curr) => accum + curr;
+    const init = 0;
+    console.log(reduce(nums, fn, init)); // 10
+
+    const nums2 = [1, 2, 3, 4];
+    const fn2 = (accum, curr) => accum + curr * curr;
+    const init2 = 100;
+    console.log(reduce(nums2, fn2, init2)); // 130
+
+    const nums3 = [];
+    const fn3 = (accum, curr) => 0;
+    const init3 = 25;
+    console.log(reduce(nums3, fn3, init3)); // 25
 })();
