2c18064d37958e3df3cacbdbae91442c6a98db65
paulAlexSerban <paul.alex.serban@gmail.com>
2023-10-06 10:51:44 +0300

    init restructuring

--- 01_programming/00_basics-and-fundamentals/04_loops/countSheeps_3.0.0.js
+++
@@ -1,6 +0,0 @@
-const countSheep = (length) => Array.from({ length }, (_, i) => ++i + " sheep...").join("");
-
-// tests
-console.log(countSheep(1));
-console.log(countSheep(2));
-console.log(countSheep(3));
